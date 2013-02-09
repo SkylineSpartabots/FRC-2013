5a41ffd103da13cd4aec6095a6485f4b7ff32d91
stevenbe99@gmail.com <stevenbe99@gmail.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2013-02-09 03:18:22 +0000

    Added the "ClimberCommands.cpp" and "ClimberCommands.h" files. "PullRobotUpCommand" is is now there and should work in the ".h" file

---
+++ trunk/Code/Commands/ClimberCommands.h
@@ -0,0 +1,24 @@
+#ifndef CLIMBER_COMMANDS_H
+#define CLIMBER_COMMANDS_H
+
+#include "WPILib.h"
+#include "..\Misc\Tools.h"
+#include "..\OperatorInterfaces\BaseOI.h"
+#include "BaseCommand.h"
+#include "..\Subsystems\Climber\ClimberArm.h"
+#include "..\subsystems\Climber\ClimberExtender.h"
+
+class PullRobotUpCommand : public Command {
+public:
+	PullRobotUpCommand (BaseClimberArm *arm);
+	~PullRobotUpCommand();
+	
+	void Initialize();
+		void Execute();
+		bool IsFinished();
+		void End();
+		void Interrupted();
+		
+private: 
+	BaseClimberArm *m_arm; 	
+};
